38c8507cdd5f8f909c942bf65d2c3f9c01e17fe5
CHENGHSIEN Lo <aszx99370765@gmail.com>
2019-04-10 15:21:59 +0800

    debug check product and check price

--- products-boss.py
+++ products-boss.py
@@ -10,17 +10,19 @@ if os.path.isfile('products.csv'): #檢查商品清單是否存在
 				continue
 			name, price = line.strip().split(',')
 			products.append([name, price])
+			choose = 1 #判斷是否要查詢價格
 	print(products)
-	add = input('是否要建立新商品清單(y/n): ')
 else: 
-	print('未找到商品清單開始建立')
+	print('未找到商品清單')
+	choose = 2
+add = input('是否要編輯清單(y/n): ')
 #建立商品清單
 if add =='y':
 	while True:
 		name = input('請輸入商品名稱: ')
 		if name == 'q' or name == 'Q':
 			break
-		elif name in products:
+		elif name in str(products):
 			print('此商品已在清單中')
 			print('')
 		else:
@@ -33,18 +35,23 @@ if add =='y':
 		file.write('商品,價格\n')
 		for p in products:
 			file.write(p[0] + ',' + str(p[1]) + '\n')
-search = input('是否需要查詢商品價格(y/n): ')
+if choose == 1:
+	search = input('是否需要查詢商品價格(y/n): ')
 
 #查詢商品價格
-if search == 'y':
-	while True:
-		productname = input('請輸入要查詢的商品: ')
-		print('')
-		if productname == 'q' or productname == 'Q':
-			break
-		with open('products.csv', 'r', encoding='utf-8') as pdlist:
-			for product in pdlist:
-				name, price = product.strip().split(',')
-				if productname in product: 
-					print('所查詢的商品: ', name, '價格:', price, '元')
-					print('')
+	if search == 'y':
+		while True:
+			productname = input('請輸入要查詢的商品: ')
+			print('')
+			if productname == 'q' or productname == 'Q':
+				break
+			with open('products.csv', 'r', encoding='utf-8') as pdlist:
+				for product in pdlist:
+					name, price = product.strip().split(',')
+					if productname in product: 
+						print('所查詢的商品: ', name, '價格:', price, '元')
+						print('')
+					else:
+						print('未有此商品')
+						print('')
+						break
