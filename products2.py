abb03b1c203b027f512a2065c2b1993eeda570fa
CHENGHSIEN Lo <aszx99370765@gmail.com>
2019-04-10 12:28:32 +0800

    add writ list second  commit

--- products2.py
+++ products2.py
@@ -4,21 +4,23 @@ while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q' or name == 'Q':
 		break
-	price = input('請輸入商品價格: ')
+	price = int(input('請輸入商品價格: '))
 	products.append([name, price]) 
 	print('')
-with open('products.csv', 'w') as file:
+with open('products.csv', 'w', encoding='utf-8') as file:
+	file.write('商品,價格\n')
 	for p in products:
-		file.write(p[0] + ',' + p[1] + '\n')
-
+		file.write(p[0] + ',' + str(p[1]) + '\n')
+search = input('是否需要查詢商品(y/n): ')
 #查詢商品價格
-while True:
-	productname = input('請輸入要查詢的商品: ')
-	print('')
-	if productname == 'q' or productname == 'Q':
-		break
-	with open('products.csv', 'r') as pdlist:
-		for product in pdlist:
-			if productname in product: 
-				print('所查詢的商品: ', product)
-				print('')
+if search == 'y':
+	while True:
+		productname = input('請輸入要查詢的商品: ')
+		print('')
+		if productname == 'q' or productname == 'Q':
+			break
+		with open('products.csv', 'r', encoding='utf-8') as pdlist:
+			for product in pdlist:
+				if productname in product: 
+					print('所查詢的商品: ', product)
+					print('')
